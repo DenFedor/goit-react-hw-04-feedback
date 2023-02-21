90af469967ab529f080ba1a269c7fe846233dfe5
DenFedor <fedorenko.wo@gmail.com>
2023-02-21 21:05:31 +0200

    Update FeedbackCounter.js

--- src/components/FeedbackCounter/FeedbackCounter.js
+++ src/components/FeedbackCounter/FeedbackCounter.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React,{useState} from 'react';
 import { Container } from './FeedbackCounter.styled';
 import { Section } from 'components/Section/Section';
 import { Statistics } from 'components/Statistics/Statistics';
@@ -6,35 +6,40 @@ import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
 import { Notification } from 'components/Notification/Notification';
 
 export const FeedbackCounterHooks = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [good, setGood]= useState(0);
+  const [neutral, setNeutral]= useState(0);
+  const [bad, setBad]= useState(0);
+ 
+const handleIncrement = e =>{
+  switch (e.target.textContent) {
+    case 'good':
+     setGood(good+1);
+      break;
+    case 'neutral':
+      setNeutral(neutral+1);
+      break;
+    case 'bad':
+      setBad(bad+1);
+      break;
 
-  const handleIncrement = e => {
-    switch (e.target.textContent) {
-      case 'good':
-        return setGood(good + 1);
-        break;
-      case 'neutral':
-        return setNeutral(neutral + 1);
-        break;
-      case 'bad':
-        return setBad(bad + 1);
-        break;
-
-      default:
-        break;
-    }
-  };
-  const countTotalFeedback = () => {
-    return good + neutral + bad;
-  };
-  const countPositiveFeedbackPercentage = () => {
-    return Math.floor((good / (good + neutral + bad)) * 100) || 0;
-  };
-  const total = countTotalFeedback();
-  const percentage = countPositiveFeedbackPercentage();
-  const options = Object.keys({ good, neutral, bad });
+    default:
+      break;
+  }
+}
+const countTotalFeedback = () => {
+  return good + neutral + bad;
+};
+const countPositiveFeedbackPercentage = () => {
+  return (Math.floor(
+      (good /
+        (good + neutral + bad)) *
+        100
+    )) || (0);
+  
+};
+const total=countTotalFeedback();
+const percentage=countPositiveFeedbackPercentage();
+const options=Object.keys({good,neutral,bad});
   return (
     <Container>
       <Section
@@ -46,25 +51,24 @@ export const FeedbackCounterHooks = () => {
           />
         }
       />
-      {total === 0 ? (
-        <Section
-          title={`Statistics`}
-          children={<Notification message={'There is no feedback'} />}
-        />
-      ) : (
-        <Section
-          title={`Statistics`}
-          children={
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={percentage}
-            />
-          }
-        />
-      )}
+      {total===0 ?<Section
+        title={`Statistics`}
+        children={
+          <Notification message= {'There is no feedback'} />
+        }
+      /> : <Section
+        title={`Statistics`}
+        children={
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={percentage}
+          />
+        }
+      /> }
     </Container>
   );
-};
+}
+
